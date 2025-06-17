42c87549154ef9a8195c084615dca7090bb88707
QuevemCruzDev <quevemcruz@gmail.com>
2025-06-17 00:01:29 -0300

    Resolução do desafio Trilha PoO desafio celular do bootcamp Dio + Wex

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("5593999-9999","15 Pro","1111111",32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
+
+Console.WriteLine("____________________");
+
+Nokia nokia = new Nokia("5593999-9999", "Tijolão das antigas", "1111111", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Firefox");
